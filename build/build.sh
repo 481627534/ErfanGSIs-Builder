1e0ebe7d985825ab7482a782586bcc9a6e944402
481627534 <63408695+481627534@users.noreply.github.com>
2020-08-28 23:03:49 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Sync
 
+SYNC_START=$(date +"%s")
+
 sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     mkdir final
+
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
+    SYNC_START=$(date +"%s")
 
     export date2=`date +%Y%m%d%H%M`
     export sourcever2=`cat ./ErfanGSIs/ver`
@@ -15,7 +21,12 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     zip -r "$ZIP_NAME"-Aonly-"$sourcever2"-"$date2"-ErfanGSI.7z *-Aonly-*.img
     zip -r "$ZIP_NAME"-AB-"$sourcever2"-"$date2"-ErfanGSI.7z *-AB-*.img
 
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
+    SYNC_START=$(date +"%s")
+
     echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ZIP_NAME-Aonly-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
     echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ZIP_NAME-AB-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
-    echo "::set-env name=DOWNLOAD_A2::$(./transfer $MIR2 "$ZIP_NAME-Aonly-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
-    echo "::set-env name=DOWNLOAD_AB2::$(./transfer $MIR2 "$ZIP_NAME-AB-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')"
+
+    SYNC_END=$(date +"%s")
+    SYNC_DIFF=$((SYNC_END - SYNC_START))
